02784ef505660f6fb78e48712f8cc79a4b47d1bd
p tanto <p tanto@DESKTOP-UDJ7LRS>
2018-08-15 19:19:32 +0700

    Coba method substring, replace, & split

--- src/jobsheet11/bagan2.java
+++ src/jobsheet11/bagan2.java
@@ -39,5 +39,19 @@ public class bagan2 {
         System.out.println("isi variable t tanpa spasi = \""+ t.trim() +"\"");
         System.out.println("gabungkan isi variable x dan r = "+ x.concat(r));
         
+         System.out.println("karakter  mulai posisi 3 di variable x = "+ x.substring(3));
+        System.out.println("karakter dari posisi 3-5 di variable x = "+ x.substring(3,6));
+        String tclean = t.trim();
+        System.out.println("isi variable tclean = "+ t.trim());
+        System.out.println("rubah 'i' jadi 'e' variable tclean = "+ tclean.replace("i","e"));
+        String[] arrT = tclean.split(" ");
+        System.out.println("pecahan kata di variable tclean = ");
+        for(int i=0;i<arrT.length;i++)
+            System.out.println(arrT[i]);
+        String[] arrTr = tclean.split("r");
+        System.out.println("pecahan berdasarkan huruf 'r' di variable tclean = ");
+        for(int i=0;i<arrTr.length;i++)
+            System.out.println(arrTr[i]);
+        
            }
 }
