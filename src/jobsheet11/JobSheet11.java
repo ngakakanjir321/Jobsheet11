dfe9f871540c2a0d406180c92ec97522bc20fa20
p tanto <p tanto@DESKTOP-UDJ7LRS>
2018-08-15 10:17:49 +0700

    intial commit

---
+++ src/jobsheet11/JobSheet11.java
@@ -0,0 +1,10 @@
+
+package jobsheet11;
+import java.util.Scanner;
+public class JobSheet11 {
+
+    public static void main(String[] args) {
+        String identitas = "Muhamad Fathoni / X RPL 2 / 20";
+        System.out.println("Identitas : "+ identitas);
+    }
+}
