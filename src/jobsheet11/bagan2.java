f25dfdd2823fa4a0c93ba57d207e097fc5aedf52
p tanto <p tanto@DESKTOP-UDJ7LRS>
2018-08-15 19:14:15 +0700

    intial commit

---
+++ src/jobsheet11/bagan2.java
@@ -0,0 +1,13 @@
+package jobsheet11;
+
+import java.util.Locale;
+
+public class bagan2 {
+    public static void main(String[] args) {
+        String identitas = "Muhamad Fathoni /XRPL 2 / 20";
+        System.out.println(" identitas : " + identitas);
+      
+        
+
+           }
+}
