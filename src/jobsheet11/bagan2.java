723a7b4d4acaeea109931c054bdeebea21a59e18
p tanto <p tanto@DESKTOP-UDJ7LRS>
2018-08-15 19:15:23 +0700

    Coba method length & isEmpty

--- src/jobsheet11/bagan2.java
+++ src/jobsheet11/bagan2.java
@@ -7,7 +7,13 @@ public class bagan2 {
         String identitas = "Muhamad Fathoni /XRPL 2 / 20";
         System.out.println(" identitas : " + identitas);
       
-        
+        String x = "OPERASI";
+        System.out.println("isi variable = "+ x);
+        System.out.println("\""+ x +"\" Panjang Character = "+ x.length ());
+        System.out.println("x adalah kosong = " + x.isEmpty());
+        String y = "";
+        System.out.println("isi variable y = "+ y);
+        System.out.println("y adalah kosong = " + y.isEmpty());
 
            }
 }
